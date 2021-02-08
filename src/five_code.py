9b2f7c71c2ad7a5f91fc368b7320a6563847170e
tamama9018 <tamama9018@gmail.com>
2021-02-08 16:08:35 +0900

    first commit

---
+++ src/five_code.py
@@ -0,0 +1,40 @@
+from key_name_matching import number_to_char
+from three_code import three_code
+from four_code import four_code
+
+def five_code(keys):
+
+    root_codename = four_code(keys[:4])
+    root = keys[0]
+    top  = keys[2]
+    add  = keys[4]
+
+    if type(root_codename) is str:
+        if add-top == 6:
+            return root_codename + '(♭9)'
+        
+        if add-top == 7:
+            return root_codename + '(9)'
+
+        if add-top == 8:
+            return root_codename + '(#9)'
+
+        if add-top == 10:
+            return root_codename + '(11)'
+
+        if add-top == 11:
+            return root_codename + '(#11)'
+
+        if add-top == 13:
+            return root_codename + '(♭13)'
+
+        if add-top == 14:
+            return root_codename + '(13)'
+
+    return keys
+
+
+
+
+
+
---
+++ src/main.py
@@ -0,0 +1,6 @@
+from codemaker import codemaker
+from key_name_matching import number_to_char
+
+keys = [0, 4, 7]
+
+print(codemaker(keys))
