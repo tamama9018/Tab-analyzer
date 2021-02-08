5d05b39dac8f84ef5c567ce3f784a386acdc5d3e
miguel-aoyama <aoyamarikiya@gmail.com>
2021-02-08 15:51:32 +0900

    file changed

--- index.php
+++ index.php
@@ -5,6 +5,64 @@
     <title>Tab-analyzer</title>
   </head>
   <body>
-    Tab-analyzer
+    <form class="neck" action="check.php" method="post">
+    <table>
+      <th></th>
+      <tr>
+        <input type="radio" name="string1" value="no-choice" checked>
+        <input type="radio" name="string1" value="1E">
+        <input type="radio" name="string1" value="1F">
+        <input type="radio" name="string1" value="1F#">
+        <input type="radio" name="string1" value="1G">
+        <input type="radio" name="string1" value="1G#">
+      </tr>
+      <br>
+      <tr>
+        <input type="radio" name="string2" value="no-choice" checked>
+        <input type="radio" name="string2" value="2B">
+        <input type="radio" name="string2" value="2C">
+        <input type="radio" name="string2" value="2C#">
+        <input type="radio" name="string2" value="2D">
+        <input type="radio" name="string2" value="2D#">
+      </tr>
+      <br>
+      <tr>
+        <input type="radio" name="string3" value="no-choice" checked>
+        <input type="radio" name="string3" value="3G">
+        <input type="radio" name="string3" value="3G#">
+        <input type="radio" name="string3" value="3A">
+        <input type="radio" name="string3" value="3A#">
+        <input type="radio" name="string3" value="3B">
+      </tr>
+      <br>
+      <tr>
+        <input type="radio" name="string4" value="no-choice" checked>
+        <input type="radio" name="string4" value="4D">
+        <input type="radio" name="string4" value="4D#">
+        <input type="radio" name="string4" value="4E">
+        <input type="radio" name="string4" value="4F">
+        <input type="radio" name="string4" value="4F#">
+      </tr>
+      <br>
+      <tr>
+        <input type="radio" name="string5" value="no-choice" checked>
+        <input type="radio" name="string5" value="5A">
+        <input type="radio" name="string5" value="5A#">
+        <input type="radio" name="string5" value="5B">
+        <input type="radio" name="string5" value="5C">
+        <input type="radio" name="string5" value="5C#">
+      </tr>
+      <br>
+      <tr>
+        <input type="radio" name="string6" value="no-choice" checked>
+        <input type="radio" name="string6" value="6E">
+        <input type="radio" name="string6" value="6F">
+        <input type="radio" name="string6" value="6F#">
+        <input type="radio" name="string6" value="6G">
+        <input type="radio" name="string6" value="6G#">
+      </tr>
+    </table>
+    <input type="submit" name="" value="Search">
+    </form>
   </body>
 </html>
