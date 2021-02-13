2e4c5411c5fd76222b864612d199d8562261335c
miguel-aoyama <aoyamarikiya@gmail.com>
2021-02-13 16:36:40 +0900

    file changed

--- index.php
+++ index.php
@@ -6,60 +6,60 @@
   </head>
   <body>
     <form class="neck" action="" method="post">
-    <table>
+    <table >
       <th></th>
       <tr>
-        <input type="radio" name="string1" value="100" checked>
-        <input type="radio" name="string1" value="4">
-        <input type="radio" name="string1" value="5">
-        <input type="radio" name="string1" value="6">
-        <input type="radio" name="string1" value="7">
-        <input type="radio" name="string1" value="8">
+        <input type="radio" name="string1" value="100" checked <?php if(isset($_POST['string1']) && $_POST['string1'] == "100")echo 'checked'; ?>>
+        <input type="radio" name="string1" value="4"<?php if(isset($_POST['string1']) && $_POST['string1'] == "4")echo 'checked'; ?>>
+        <input type="radio" name="string1" value="5"<?php if(isset($_POST['string1']) && $_POST['string1'] == "5")echo 'checked'; ?>>
+        <input type="radio" name="string1" value="6"<?php if(isset($_POST['string1']) && $_POST['string1'] == "6")echo 'checked'; ?>>
+        <input type="radio" name="string1" value="7"<?php if(isset($_POST['string1']) && $_POST['string1'] == "7")echo 'checked'; ?>>
+        <input type="radio" name="string1" value="8"<?php if(isset($_POST['string1']) && $_POST['string1'] == "8")echo 'checked'; ?>>
       </tr>
       <br>
       <tr>
-        <input type="radio" name="string2" value="101" checked>
-        <input type="radio" name="string2" value="11">
-        <input type="radio" name="string2" value="0">
-        <input type="radio" name="string2" value="1">
-        <input type="radio" name="string2" value="2">
-        <input type="radio" name="string2" value="3">
+        <input type="radio" name="string2" value="101" checked<?php if(isset($_POST['string2']) && $_POST['string2'] == "101")echo 'checked'; ?>>
+        <input type="radio" name="string2" value="11" <?php if(isset($_POST['string2']) && $_POST['string2'] == "11")echo 'checked'; ?>>
+        <input type="radio" name="string2" value="0" <?php if(isset($_POST['string2']) && $_POST['string2'] == "0")echo 'checked'; ?>>
+        <input type="radio" name="string2" value="1" <?php if(isset($_POST['string2']) && $_POST['string2'] == "1")echo 'checked'; ?>>
+        <input type="radio" name="string2" value="2" <?php if(isset($_POST['string2']) && $_POST['string2'] == "2")echo 'checked'; ?>>
+        <input type="radio" name="string2" value="3" <?php if(isset($_POST['string2']) && $_POST['string2'] == "3")echo 'checked'; ?>>
       </tr>
       <br>
       <tr>
-        <input type="radio" name="string3" value="102" checked>
-        <input type="radio" name="string3" value="7">
-        <input type="radio" name="string3" value="8">
-        <input type="radio" name="string3" value="9">
-        <input type="radio" name="string3" value="10">
-        <input type="radio" name="string3" value="11">
+        <input type="radio" name="string3" value="102" checked <?php if(isset($_POST['string3']) && $_POST['string3'] == "102")echo 'checked'; ?>>
+        <input type="radio" name="string3" value="7" <?php if(isset($_POST['string3']) && $_POST['string3'] == "7")echo 'checked'; ?>>
+        <input type="radio" name="string3" value="8" <?php if(isset($_POST['string3']) && $_POST['string3'] == "8")echo 'checked'; ?>>
+        <input type="radio" name="string3" value="9" <?php if(isset($_POST['string3']) && $_POST['string3'] == "9")echo 'checked'; ?>>
+        <input type="radio" name="string3" value="10" <?php if(isset($_POST['string3']) && $_POST['string3'] == "10")echo 'checked'; ?>>
+        <input type="radio" name="string3" value="11" <?php if(isset($_POST['string3']) && $_POST['string3'] == "11")echo 'checked'; ?>>
       </tr>
       <br>
       <tr>
-        <input type="radio" name="string4" value="103" checked>
-        <input type="radio" name="string4" value="2">
-        <input type="radio" name="string4" value="3">
-        <input type="radio" name="string4" value="4">
-        <input type="radio" name="string4" value="5">
-        <input type="radio" name="string4" value="6">
+        <input type="radio" name="string4" value="103" checked <?php if(isset($_POST['string4']) && $_POST['string4'] == "103")echo 'checked'; ?>>
+        <input type="radio" name="string4" value="2" <?php if(isset($_POST['string4']) && $_POST['string4'] == "2")echo 'checked'; ?>>
+        <input type="radio" name="string4" value="3" <?php if(isset($_POST['string4']) && $_POST['string4'] == "3")echo 'checked'; ?>>
+        <input type="radio" name="string4" value="4" <?php if(isset($_POST['string4']) && $_POST['string4'] == "4")echo 'checked'; ?>>
+        <input type="radio" name="string4" value="5" <?php if(isset($_POST['string4']) && $_POST['string4'] == "5")echo 'checked'; ?>>
+        <input type="radio" name="string4" value="6" <?php if(isset($_POST['string4']) && $_POST['string4'] == "6")echo 'checked'; ?>>
       </tr>
       <br>
       <tr>
-        <input type="radio" name="string5" value="104" checked>
-        <input type="radio" name="string5" value="9">
-        <input type="radio" name="string5" value="10">
-        <input type="radio" name="string5" value="11">
-        <input type="radio" name="string5" value="0">
-        <input type="radio" name="string5" value="1">
+        <input type="radio" name="string5" value="104" checked <?php if(isset($_POST['string5']) && $_POST['string5'] == "104")echo 'checked'; ?>>
+        <input type="radio" name="string5" value="9" <?php if(isset($_POST['string5']) && $_POST['string5'] == "9")echo 'checked'; ?>>
+        <input type="radio" name="string5" value="10" <?php if(isset($_POST['string5']) && $_POST['string5'] == "10")echo 'checked'; ?>>
+        <input type="radio" name="string5" value="11" <?php if(isset($_POST['string5']) && $_POST['string5'] == "11")echo 'checked'; ?>>
+        <input type="radio" name="string5" value="0" <?php if(isset($_POST['string5']) && $_POST['string5'] == "0")echo 'checked'; ?>>
+        <input type="radio" name="string5" value="1" <?php if(isset($_POST['string5']) && $_POST['string5'] == "1")echo 'checked'; ?>>
       </tr>
       <br>
       <tr>
-        <input type="radio" name="string6" value="105" checked>
-        <input type="radio" name="string6" value="4">
-        <input type="radio" name="string6" value="5">
-        <input type="radio" name="string6" value="6">
-        <input type="radio" name="string6" value="7">
-        <input type="radio" name="string6" value="8">
+        <input type="radio" name="string6" value="105" checked <?php if(isset($_POST['string6']) && $_POST['string6'] == "105")echo 'checked'; ?>>
+        <input type="radio" name="string6" value="4" <?php if(isset($_POST['string6']) && $_POST['string6'] == "4")echo 'checked'; ?>>
+        <input type="radio" name="string6" value="5" <?php if(isset($_POST['string6']) && $_POST['string6'] == "5")echo 'checked'; ?>>
+        <input type="radio" name="string6" value="6" <?php if(isset($_POST['string6']) && $_POST['string6'] == "6")echo 'checked'; ?>>
+        <input type="radio" name="string6" value="7" <?php if(isset($_POST['string6']) && $_POST['string6'] == "7")echo 'checked'; ?>>
+        <input type="radio" name="string6" value="8" <?php if(isset($_POST['string6']) && $_POST['string6'] == "8")echo 'checked'; ?>>
       </tr>
     </table>
     <input type="submit" name="" value="Search">
@@ -71,6 +71,7 @@
     $string4 = $_POST['string4'];
     $string5 = $_POST['string5'];
     $string6 = $_POST['string6'];
+
     $strings = [];
     $strings[] = (int)$string1;
     $strings[] = (int)$string2;
