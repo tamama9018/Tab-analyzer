0982868d092feb6ee81851132179090dbd99e324
miguel-aoyama <aoyamarikiya@gmail.com>
2021-04-27 18:13:54 +0900

    file changed

---
+++ pianotest.js
@@ -0,0 +1,175 @@
+
+//string1
+function showimg1(){
+  radio = document.getElementsByName('string1')
+  if(radio[1].checked){
+    document.area1.src = "images/e.png";
+  }else if (radio[2].checked) {
+    document.area1.src = "images/f.png";
+  }else if (radio[3].checked) {
+    document.area1.src = "images/fs.png";
+  }else if (radio[4].checked) {
+    document.area1.src = "images/g.png";
+  }else if (radio[5].checked) {
+    document.area1.src = "images/gs.png";
+  }else if (radio[6].checked) {
+    document.area1.src = "images/a.png";
+  }else if (radio[7].checked) {
+    document.area1.src = "images/as.png";
+  }else if (radio[8].checked) {
+    document.area1.src = "images/b.png";
+  }else if (radio[9].checked) {
+    document.area1.src = "images/c.png";
+  }else if (radio[10].checked) {
+    document.area1.src = "images/cs.png";
+  }else if (radio[11].checked) {
+    document.area1.src = "images/d.png";
+  }else if (radio[12].checked) {
+    document.area1.src = "images/ds.png";
+  }
+}
+//string2
+function showimg2(){
+  radio = document.getElementsByName('string2')
+  if(radio[1].checked){
+    document.area2.src = "images/b.png";
+  }else if (radio[2].checked) {
+    document.area2.src = "images/c.png";
+  }else if (radio[3].checked) {
+    document.area2.src = "images/cs.png";
+  }else if (radio[4].checked) {
+    document.area2.src = "images/d.png";
+  }else if (radio[5].checked) {
+    document.area2.src = "images/ds.png";
+  }else if (radio[6].checked) {
+    document.area2.src = "images/e.png";
+  }else if (radio[7].checked) {
+    document.area2.src = "images/f.png";
+  }else if (radio[8].checked) {
+    document.area2.src = "images/fs.png";
+  }else if (radio[9].checked) {
+    document.area2.src = "images/g.png";
+  }else if (radio[10].checked) {
+    document.area2.src = "images/gs.png";
+  }else if (radio[11].checked) {
+    document.area2.src = "images/a.png";
+  }else if (radio[12].checked) {
+    document.area2.src = "images/as.png";
+  }
+}
+//string3
+function showimg3(){
+  radio = document.getElementsByName('string3')
+  if(radio[1].checked){
+    document.area3.src = "images/g.png";
+  }else if (radio[2].checked) {
+    document.area3.src = "images/gs.png";
+  }else if (radio[3].checked) {
+    document.area3.src = "images/a.png";
+  }else if (radio[4].checked) {
+    document.area3.src = "images/as.png";
+  }else if (radio[5].checked) {
+    document.area3.src = "images/b.png";
+  }else if (radio[6].checked) {
+    document.area3.src = "images/c.png";
+  }else if (radio[7].checked) {
+    document.area3.src = "images/cs.png";
+  }else if (radio[8].checked) {
+    document.area3.src = "images/d.png";
+  }else if (radio[9].checked) {
+    document.area3.src = "images/ds.png";
+  }else if (radio[10].checked) {
+    document.area3.src = "images/e.png";
+  }else if (radio[11].checked) {
+    document.area3.src = "images/f.png";
+  }else if (radio[12].checked) {
+    document.area3.src = "images/fs.png";
+  }
+}
+//string4
+function showimg4(){
+  radio = document.getElementsByName('string4')
+  if(radio[1].checked){
+    document.area4.src = "images/d.png";
+  }else if (radio[2].checked) {
+    document.area4.src = "images/ds.png";
+  }else if (radio[3].checked) {
+    document.area4.src = "images/e.png";
+  }else if (radio[4].checked) {
+    document.area4.src = "images/f.png";
+  }else if (radio[5].checked) {
+    document.area4.src = "images/fs.png";
+  }else if (radio[6].checked) {
+    document.area4.src = "images/g.png";
+  }else if (radio[7].checked) {
+    document.area4.src = "images/gs.png";
+  }else if (radio[8].checked) {
+    document.area4.src = "images/a.png";
+  }else if (radio[9].checked) {
+    document.area4.src = "images/as.png";
+  }else if (radio[10].checked) {
+    document.area4.src = "images/b.png";
+  }else if (radio[11].checked) {
+    document.area4.src = "images/c.png";
+  }else if (radio[12].checked) {
+    document.area4.src = "images/cs.png";
+  }
+}
+//string5
+function showimg5(){
+  radio = document.getElementsByName('string5')
+  if(radio[1].checked){
+    document.area5.src = "images/a.png";
+  }else if (radio[2].checked) {
+    document.area5.src = "images/as.png";
+  }else if (radio[3].checked) {
+    document.area5.src = "images/b.png";
+  }else if (radio[4].checked) {
+    document.area5.src = "images/c.png";
+  }else if (radio[5].checked) {
+    document.area5.src = "images/cs.png";
+  }else if (radio[6].checked) {
+    document.area5.src = "images/d.png";
+  }else if (radio[7].checked) {
+    document.area5.src = "images/ds.png";
+  }else if (radio[8].checked) {
+    document.area5.src = "images/e.png";
+  }else if (radio[9].checked) {
+    document.area5.src = "images/f.png";
+  }else if (radio[10].checked) {
+    document.area5.src = "images/fs.png";
+  }else if (radio[11].checked) {
+    document.area5.src = "images/g.png";
+  }else if (radio[12].checked) {
+    document.area5.src = "images/gs.png";
+  }
+}
+//string6
+function showimg6(){
+  radio = document.getElementsByName('string6')
+  if(radio[1].checked){
+    document.area6.src = "images/e.png";
+  }else if (radio[2].checked) {
+    document.area6.src = "images/f.png";
+  }else if (radio[3].checked) {
+    document.area6.src = "images/fs.png";
+  }else if (radio[4].checked) {
+    document.area6.src = "images/g.png";
+  }else if (radio[5].checked) {
+    document.area6.src = "images/gs.png";
+  }else if (radio[6].checked) {
+    document.area6.src = "images/a.png";
+  }else if (radio[7].checked) {
+    document.area6.src = "images/as.png";
+  }else if (radio[8].checked) {
+    document.area6.src = "images/b.png";
+  }else if (radio[9].checked) {
+    document.area6.src = "images/c.png";
+  }else if (radio[10].checked) {
+    document.area6.src = "images/cs.png";
+  }else if (radio[11].checked) {
+    document.area6.src = "images/d.png";
+  }else if (radio[12].checked) {
+    document.area6.src = "images/ds.png";
+  }
+}
