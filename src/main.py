567f937ef99a786dbecaeb0c8654732970c7c15f
tamama9018 <tamama9018@gmail.com>
2021-05-17 23:13:19 +0900

    1~13

--- src/main.py
+++ src/main.py
@@ -4,7 +4,10 @@ from key_name_matching import number_to_char
 
 if __name__ == "__main__":
 
-    keys = [*sys.argv[1:]]
-    keys = [int(key) for key in keys]
-    
+    input_keys = [*sys.argv[1:]]
+    keys = []
+    for key in input_keys:
+        if key != '0':
+            keys.append(int(key)-1)
+  #  keys = [int(key) for key in keys]
     print(codemaker(keys))
