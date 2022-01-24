e3e23e1854bad3e6b0ee6040762fd306fe8d081f
木村勇輝 <Yutyo@MyMac.local>
2022-01-24 17:33:43 +0900

    sus4(7) -> 7sus4

--- src/code_rename.py
+++ src/code_rename.py
@@ -9,7 +9,13 @@ def code_rename(code):
     code = code.replace('ga', 'g_a')
     code = code.replace('47', '4(7)')
     code = code.replace('27', '2(7)')
-
+    code = code.replace('sus2(7)', '7sus2')
+    code = code.replace('sus2M7', 'M7sus2')
+    code = code.replace('sus4(7)', '7sus4')
+    code = code.replace('sus4M7', 'M7sus4')
+    code = code.replace('sus2(6)', '6sus2')
+    code = code.replace('sus4(6)', '6sus4')
+    
     return code
     
 
